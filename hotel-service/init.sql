4baf964399d9c62cc35fa8c7c3d1e1f50d66f917
niteshops01 <niteshops)1@gmail.com>
2025-05-11 13:19:10 +0000

    feat: added service data

--- hotel-service/init.sql
+++ hotel-service/init.sql
@@ -4,13 +4,31 @@ BEGIN
         CREATE USER postgres WITH PASSWORD 'admin123';
     END IF;
 END$$;
+
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'hoteldb') THEN
         CREATE DATABASE hoteldb;
     END IF;
 END$$;
+
 GRANT ALL PRIVILEGES ON DATABASE hoteldb TO postgres;
 \c hoteldb
 GRANT ALL PRIVILEGES ON SCHEMA public TO postgres;
 GRANT CREATE ON DATABASE hoteldb TO postgres;
+
+GRANT USAGE ON SCHEMA public TO postgres;
+GRANT CREATE ON SCHEMA public TO postgres;
+
+ALTER DATABASE hoteldb OWNER TO postgres;
+
+-- Check if the 'hotels' table exists before altering its ownership.
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'hotels' AND table_schema = 'public') THEN
+        ALTER TABLE hotels OWNER TO postgres;
+    END IF;
+END$$;
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES TO postgres;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON SEQUENCES TO postgres;
